e19610578023532d10b8969a49f1cca3d6e63b09
tqbao98 <34277269+tqbao98@users.noreply.github.com>
2017-12-05 17:34:02 +0200

    Add files via upload

---
+++ BUFFER.cpp
@@ -0,0 +1,253 @@
+#include "BUFFER.h"
+#include <boost/asio.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <algorithm>
+#include <iterator>
+#include <boost/lexical_cast.hpp>
+#include <iostream>
+#include <boost/bind.hpp>
+
+//reference data types changed
+//use fixed values for hostname, port and timelimit -> eliminate parameters??
+SickTimCommonTcp::SickTimCommonTcp(int timelimit) 
+	:
+	socket_(io_service_),
+	deadline_(io_service_),
+	timelimit_(timelimit)
+{
+	deadline_.expires_at(boost::posix_time::pos_infin);
+	checkDeadline();
+}
+
+SickTimCommonTcp::~SickTimCommonTcp()
+{
+	close_device();
+}
+
+using boost::asio::ip::tcp;
+using boost::lambda::var;
+using boost::lambda::_1;
+
+int SickTimCommonTcp::init_device()
+{
+	// Resolve the supplied hostname
+	tcp::resolver::iterator iterator;
+	try
+	{
+		tcp::resolver resolver(io_service_);
+		tcp::resolver::query query("169.254.138.124", "2112");
+		iterator = resolver.resolve(query);
+	}
+	catch (boost::system::system_error &e)
+	{
+		cout << "error in init TCP" << &e << endl;
+		return -1;
+	}
+
+	// Try to connect to all possible endpoints
+	boost::system::error_code ec;
+	bool success = false;
+	for (; iterator != tcp::resolver::iterator(); ++iterator)
+	{
+		std::string repr = boost::lexical_cast<std::string>(iterator->endpoint());
+		socket_.close();
+
+		//// Set the time out length
+		//cout << "waiting time out " << timelimit_ << endl;
+		//deadline_.expires_from_now(boost::posix_time::seconds(/*timelimit_*/5000));
+		//ec = boost::asio::error::would_block;
+		
+		cout << "connecting to " << repr.c_str() << endl;
+		boost::asio::connect(socket_, iterator); //asynchronous
+
+		//// Wait until timeout
+		//do io_service_.run_one(); while (ec == boost::asio::error::would_block);
+
+		if (!ec && socket_.is_open())
+		{
+			success = true;
+			cout << "connected" << endl;
+			break;
+		}
+		cout << "failed to connect to " << repr.c_str() << endl;
+	}
+
+	// Check if connecting succeeded
+	if (!success)
+	{
+		cout << "cannot connect to " << hostname_.c_str() << port_.c_str() << endl;
+		return -1;
+	}
+
+	input_buffer_.consume(input_buffer_.size());
+
+	return 0;
+}
+
+int SickTimCommonTcp::close_device()
+{
+	if (socket_.is_open())
+	{
+		try
+		{
+			socket_.close();
+		}
+		catch (boost::system::system_error &e)
+		{
+			cout << "can't close " << e.code().value() << e.code().message().c_str() << endl;
+		}
+	}
+	return 0;
+}
+
+void SickTimCommonTcp::checkDeadline()
+{
+	if (deadline_.expires_at() <= boost::asio::deadline_timer::traits_type::now())
+	{
+		// The reason the function is called is that the deadline expired. Close
+		// the socket to return all IO operations and reset the deadline
+		socket_.close();
+		deadline_.expires_at(boost::posix_time::pos_infin);
+	}
+
+	// Nothing bad happened, go back to sleep
+	deadline_.async_wait(boost::bind(&SickTimCommonTcp::checkDeadline, this));
+}
+
+int SickTimCommonTcp::readWithTimeout(size_t timeout_ms, char *buffer, int buffer_size, int *bytes_read, bool *exception_occured)
+{
+	// Set up the deadline to the proper timeout, error and delimiters
+	deadline_.expires_from_now(boost::posix_time::milliseconds(timeout_ms));
+	const char end_delim = static_cast<char>(0x03);
+	ec_ = boost::asio::error::would_block;
+	bytes_transfered_ = 0;
+
+	// Read until 0x03 ending indicator
+	boost::asio::async_read_until(
+		socket_,
+		input_buffer_,
+		end_delim,
+		boost::bind(
+			&SickTimCommonTcp::handleRead,
+			this,
+			boost::asio::placeholders::error,
+			boost::asio::placeholders::bytes_transferred
+		)
+	);
+	do io_service_.run_one(); while (ec_ == boost::asio::error::would_block);
+
+	if (ec_)
+	{
+		// would_block means the connectio is ok, but nothing came in in time.
+		// If any other error code is set, this means something bad happened.
+		if (ec_ != boost::asio::error::would_block)
+		{
+			cout << "sendSOPASCommand: failed attempt to read from socket: %d: %s" << ec_.value() << ec_.message().c_str() <<endl;
+			if (exception_occured != 0)
+				*exception_occured = true;
+		}
+
+		// For would_block, just return and indicate nothing bad happend
+		return -1;
+	}
+
+	// Avoid a buffer overflow by limiting the data we read
+	size_t to_read = bytes_transfered_ > buffer_size - 1 ? buffer_size - 1 : bytes_transfered_;
+	size_t i = 0;
+	std::istream istr(&input_buffer_);
+	if (buffer != 0)
+	{
+		istr.read(buffer, to_read);
+		buffer[to_read] = 0;
+
+		// Consume the rest of the message if necessary
+		if (to_read < bytes_transfered_)
+		{
+			cout << "Dropping "<< bytes_transfered_ - to_read << " bytes to avoid buffer overflow"<< endl;
+			input_buffer_.consume(bytes_transfered_ - to_read);
+		}
+	}
+	else
+		// No buffer was provided, just drop the data
+		input_buffer_.consume(bytes_transfered_);
+
+	// Set the return variable to the size of the read message
+	if (bytes_read != 0)
+		*bytes_read = to_read;
+
+	return -1;
+}
+
+/**
+* Send a SOPAS command to the device and print out the response to the console.
+*/
+int SickTimCommonTcp::sendSOPASCommand(const char* request/*, std::vector<unsigned char> * reply*/)
+{
+	if (!socket_.is_open()) {
+		cout << "sendSOPASCommand: socket not open" << endl;
+		return -1;
+	}
+
+	/*
+	* Write a SOPAS variable read request to the device.
+	*/
+	try
+	{
+		boost::asio::write(socket_, boost::asio::buffer(request, strlen(request)));
+	}
+	catch (boost::system::system_error &e)
+	{
+		cout << "write error for command: %s" << request << endl;
+		return -1;
+	}
+	// Set timeout in 5 seconds
+	const int BUF_SIZE = 1000;
+	char buffer[BUF_SIZE];
+	int bytes_read;
+	if (readWithTimeout(1000, buffer, BUF_SIZE, &bytes_read, 0) == -1)
+	{
+		cout << "sendSOPASCommand: no full reply available for read after 1s" << endl;
+		return -1;
+	}
+
+	//if (reply)
+	//{
+	//	reply->resize(bytes_read);
+	//	std::copy(buffer, buffer + bytes_read, &(*reply)[0]);
+	//}
+
+	return 0;
+}
+
+int SickTimCommonTcp::get_datagram(char* receiveBuffer, int bufferSize, int* actual_length)
+{
+	if (!socket_.is_open()) {
+		cout << "datagram: socket not open" << endl;
+		return -1;
+	}
+
+	/*
+	* Write a SOPAS variable read request to the device.
+	*/
+	std::vector<unsigned char> reply;
+
+	// Wait at most 1000ms for a new scan
+	size_t timeout = 1000;
+	bool exception_occured = false;
+
+	char *buffer = reinterpret_cast<char *>(receiveBuffer);
+
+	if (readWithTimeout(timeout, buffer, bufferSize, actual_length, &exception_occured) != 0)
+	{
+		cout << "get_datagram: no data available for read after "<< timeout << " ms" << endl;
+			// Attempt to reconnect when the connection was terminated
+		if (!socket_.is_open())
+		{
+			cout << "reconnecting" << endl;
+		}
+
+		return exception_occured ? -1 : 0;  // keep on trying
+	}
+
+	return 0;
+}
---
+++ parser.cpp
@@ -0,0 +1,170 @@
+#include "parser.h"
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+static int ReadNextField(char* source_buffer, int source_length, int index, char* store_buffer)
+{
+	int c = index;
+	while (c < source_length && (source_buffer[c] != '_') && !isalnum(source_buffer[c])) {
+		++c;
+	}
+	int len = 0;
+	while ((isalnum(source_buffer[c]) || source_buffer[c] == '_') && c < source_length && source_buffer[c] != 0x03 && source_buffer[c] != 0x00) {
+		store_buffer[len++] = source_buffer[c++];
+	}
+	store_buffer[len] = '\0';
+	if (source_buffer[c] == 0x03) {
+		return -1;
+	}
+	/*DEBUG_PRINT(store_buffer);*/
+	return c;
+}
+
+ScanDataFrame::ScanDataFrame()
+	: m_points(NULL)
+{
+}
+
+ScanDataFrame::ScanDataFrame(const ScanDataFrame& new_data)
+	: m_time_stamp(new_data.m_time_stamp),
+	m_start_angle(new_data.m_start_angle),
+	m_end_angle(new_data.m_end_angle),
+	m_angular_step(new_data.m_angular_step),
+	m_num_points(new_data.m_num_points)
+{
+	m_points = new data_point_t[new_data.m_num_points];
+	for (int i = 0; i < new_data.m_num_points; ++i) {
+		m_points[i] = new_data.m_points[i];
+	}
+}
+
+ScanDataFrame::~ScanDataFrame()
+{
+	delete[] m_points;
+}
+
+
+void ScanDataFrame::ParseDatagram(char* buffer, int buffer_length)
+{
+	std::vector<std::string> fields(0);
+	/*cout << buffer << endl;
+	cout << buffer_length << endl;*/
+	int k = 0;
+	int num_fields = 0;
+	while (k < buffer_length && k != -1) {
+		char* s = new char[MAXSIZE + 1];
+		k = ReadNextField(buffer, buffer_length, k, s);
+		if (k >= buffer_length) {
+			cout << "break" << endl;
+			break;
+		}
+		fields.push_back(std::string(s));
+		++num_fields;
+		delete[] s;
+	}
+
+	int32_t temp;
+
+	/*char* s = new char[1000];*/
+	//  Utils::GetTimeString(&m_time_stamp, s);
+	//  DEBUG_PRINT(" + Time: %s", s);
+
+	sscanf(fields[23].c_str(), "%x", &temp);
+	m_start_angle = (float)temp / 10000;
+	cout << " + Start angle: " << m_start_angle << endl;
+
+	sscanf(fields[24].c_str(), "%x", &temp);
+	m_angular_step = (float)temp / 10000;
+	cout << " + Angular step: " << m_angular_step << endl;
+
+	sscanf(fields[25].c_str(), "%x", &m_num_points);
+	cout << " + Number of data points: " << m_num_points << endl;
+
+	m_end_angle = m_start_angle + (m_num_points - 1)*m_angular_step;
+	cout << " + End angle: " << m_end_angle << endl;
+
+	m_points = new data_point_t[m_num_points];
+	int offset = 26;
+	for (int i = 0; i < m_num_points; ++i) {
+		sscanf(fields[offset + i].c_str(), "%x", &temp);
+		m_points[i].range = (uint16_t)temp;
+		DEBUG_PRINT("range[%d] = %d", i, m_points[i].range);
+		m_points[i].intensity = 0;
+	}
+	for (; offset < (int)fields.size(); ++offset) {
+		if (strcmp("RSSI1", fields[offset].c_str()) == 0) {
+			break;
+		}
+	}
+	if (offset < (int)fields.size()) { // RSSI1 included in the message
+		for (int i = 0; i < m_num_points; ++i) {
+			sscanf(fields[offset + i].c_str(), "%x", &temp);
+			m_points[i].intensity = (uint8_t)temp;
+			DEBUG_PRINT("intensity[%d] = %d", i, m_points[i].intensity);
+		}
+	}
+
+	/*delete[] s;*/
+}
+
+bool ScanDataFrame::ReadFromFile(const char* filename)
+{
+	std::ifstream in_file;
+	in_file.open(filename, std::ios_base::binary);
+	if (!in_file.is_open()) {
+		DEBUG_PRINT("Failed to open file %s!", filename);
+		return false;
+	}
+	in_file.read((char*)this, sizeof(ScanDataFrame) - sizeof(data_point_t*));
+	m_points = new data_point_t[m_num_points];
+	for (int i = 0; i < m_num_points; ++i) {
+		in_file.read((char*)&m_points[i], sizeof(data_point_t));
+	}
+	in_file.close();
+	return true;
+}
+
+bool ScanDataFrame::SaveToFile(const char* filename)
+{
+	std::ofstream out_file;
+	out_file.open(filename, std::ios_base::binary);
+	if (!out_file.is_open()) {
+		return false;
+	}
+	out_file.write((const char*)this, sizeof(ScanDataFrame) - sizeof(data_point_t*));
+	out_file.write((const char*)m_points, sizeof(data_point_t)*m_num_points);
+	out_file.close();
+	return true;
+}
+
+bool ScanDataFrame::Section(bool trigger) {
+		for (int i = 350; i <= 450; i++) {
+			if (m_points[i].range <= 150 && m_points[i].range >= 30) {
+				cout << "Hello hello hello" << endl;
+				trigger = true; //global var
+				break;
+				}
+				else trigger = false;
+			}
+		return trigger;
+}
+
+bool ScanDataFrame::Timer(bool memory, bool turnoff) {
+	time(&start);
+	while ((double)(clock() - start) / CLOCKS_PER_SEC <= 3)
+	{
+		if (turnoff == true) {
+			start = 4;
+			turnoff = false;
+			memory = false;
+			break;
+		}
+	}
+	cout << "It's opened dude";
+	system("xset dpms force on");
+	memory = false;
+	return memory, turnoff;
+}
+
