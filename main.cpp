80cf9e8b5b3689a1e7e7e166384962d0b9fd28e2
tqbao98 <34277269+tqbao98@users.noreply.github.com>
2017-12-05 17:33:25 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "buffer.h"
+#include "parser.h"
+#include <iostream>
+#include <thread>
+
+int main() {
+	
+	
+		/*unsigned char* receiveBuffer = new char[65536];*/
+	bool maintrigger = false, mainmemory = false, mainturnoff = true;
+	system("xset dpms force off");
+	
+
+
+	for (int loop = 0; loop = 1500; loop++) {
+		int actual_length = 0;
+		char* receiveBuffer = new char[65536];
+		ScanDataFrame data;
+		SickTimCommonTcp myTcp(5000);
+
+			myTcp.init_device();
+			myTcp.sendSOPASCommand("\x02sEN  LMDscandata 1\x03\0");
+			//myTcp.sendSOPASCommand("\x02sWN LMPoutputRange 1 2710 FFF92230 225510\x03\0");
+
+			int result = myTcp.get_datagram(receiveBuffer, 65536, &actual_length);
+
+			cout << receiveBuffer << endl;
+
+			data.ParseDatagram(receiveBuffer, actual_length);
+			data.Section(maintrigger);
+			if (maintrigger == true) {
+				if (mainmemory == false) {
+					thread ThrObj(&ScanDataFrame::Timer, data, mainmemory, mainturnoff);
+					ThrObj.detach();
+				}
+			} mainturnoff = true;
+
+
+			delete[] receiveBuffer;
+
+			if (actual_length <= 0) return main();
+
+			myTcp.close_device();
+		}
+		return 0;
+}
